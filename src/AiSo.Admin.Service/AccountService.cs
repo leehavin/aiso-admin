000be0451ba6366fe3c60485ffdf9cafd85b537d
李海维 <leehavin@outlook.com>
2025-03-10 17:08:15 +0800

    feat:add refresh token

--- src/AiSo.Admin.Service/AccountService.cs
+++ src/AiSo.Admin.Service/AccountService.cs
@@ -1,8 +1,12 @@
 ﻿using AiSo.Admin.Repository;
+using AiSo.Admin.Service.Dtos.JwtToken;
 using AiSo.Admin.Service.Dtos.User;
 using AiUo;
+using AiUo.CliWrap;
 using AiUo.Data.SqlSugar;
+using AiUo.Net;
 using AiUo.Security;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AiSo.Admin.Service;
 
@@ -24,9 +28,7 @@ public class AccountService
         if (!isMatch)
             throw new CustomException("LOGIN_ERROR", "密码错误");
 
-
         var jwtToken = JwtUtil.CreateJwtToken(user.Id, string.Empty, string.Empty);
-
         return new LoginResponseDto()
         {
             UserId = user.Id,
@@ -36,5 +38,33 @@ public class AccountService
             Mobile = user.Mobile,
             JwtToken = jwtToken
         };
+    }
+
+    /// <summary>
+    /// 刷新Token
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    /// <exception cref="CustomException"></exception>
+    public async Task<string> RefreshToken(RefreshJwtTokenRequestDto dto)
+    {
+        if (string.IsNullOrEmpty(dto.JwtToken))
+            new Exception("JwtToken不能为空");
+
+        if (dto.UserId is 0L)
+            new Exception("UserId不能为空");
+
+        var jwt = JwtUtil.ReadJwtToken(dto.JwtToken, string.Empty);
+        if (jwt.Status == JwtTokenStatus.Invalid || jwt.UserId != dto.UserId.ToString())
+            throw new CustomException(GResponseCodes.G_JWT_TOKEN_INVALID, "无效的token");
+
+        if (jwt.Status == JwtTokenStatus.Expired)
+            throw new CustomException(GResponseCodes.G_JWT_TOKEN_EXPIRED, "过期的token");
+
+        var user = await DbUtil.GetDbById().Queryable<SysUserEntity>().FirstAsync(f => f.Id == dto.UserId);
+        if (user == null)
+            throw new CustomException(GResponseCodes.G_JWT_TOKEN_INVALID, "无效的用户");
+
+        return JwtUtil.CreateJwtToken(dto.UserId, jwt.RoleString, string.Empty);
     }
 }
---
+++ src/AiSo.Admin.Service/Dtos/JwtToken/RefreshJwtTokenRequestDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AiSo.Admin.Service.Dtos.JwtToken;
+
+public class RefreshJwtTokenRequestDto
+{
+    /// <summary>
+    /// Token
+    /// </summary>
+    public string JwtToken { get; set; }
+
+    /// <summary>
+    /// 用户主键
+    /// </summary>
+    public long UserId { get; set; }
+}
